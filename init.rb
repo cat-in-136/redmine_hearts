a7ce7b1028aebf2da74581d8122dc43c5ee829cb
cat_in_136 <cat.in.136+github@gmail.com>
2018-02-17 14:09:55 +0900

    version v0.2.0

--- init.rb
+++ init.rb
@@ -25,7 +25,7 @@ Redmine::Plugin.register :redmine_hearts do
   name 'Redmine Hearts plugin'
   author '@cat_in_136'
   description 'provide intra-Redmine Like/Fav reactions'
-  version '0.1.0'
+  version '0.2.0'
   url 'https://github.com/cat-in-136/redmine_hearts'
   author_url 'https://github.com/cat-in-136/'
 
