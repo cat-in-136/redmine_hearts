ba78cb6d2c59ae001590224a7e2422f185469f44
cat_in_136 <cat.in.136+github@gmail.com>
2018-06-10 14:35:27 +0900

    introduce heart count to the issue query filter cat-in-136/redmine_hearts#13.
    The N + 1 problem is left due to the original design/code of Redmine.

--- init.rb
+++ init.rb
@@ -18,6 +18,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require_dependency 'redmine_hearts/acts_as_heartable.rb'
+require_dependency 'redmine_hearts/issue_query.rb'
 require_dependency 'redmine_hearts/redmine_heartable_patch.rb'
 require_dependency 'redmine_hearts/view_hook.rb'
 
