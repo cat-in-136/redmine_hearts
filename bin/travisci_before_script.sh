4d21f544fab496e93b85ffa690e58fda78818706
cat_in_136 <cat.in.136+github@gmail.com>
2019-06-02 10:26:31 +0900

    apply redmine defect 30353 for travis-ci script
    https://www.redmine.org/issues/30353

--- bin/travisci_before_script.sh
+++ bin/travisci_before_script.sh
@@ -7,6 +7,12 @@ export RAILS_ENV=test
 #REDMINE_VERSION="2.4.0"
 PLUGIN_NAME=redmine_hearts
 
+# https://www.redmine.org/issues/30353
+if [ "v${REDMINE_VERSION:0:2}" = "v3." ]; then
+  gem uninstall bundler -v ">= 2.0" 
+  gem install bundler -v "< 2.0" 
+fi
+
 cd redmine
 
 bundle install --path=${BUNDLE_PATH:-vendor/bundle}
