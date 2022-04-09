606d801e2022d5bd331d9b851838415ec7b46a0e
cat_in_136 <cat.in.136+github@gmail.com>
2022-04-10 07:27:19 +0900

    Support for Zeitwerk/Rails6
    replace ActiveSupport::Reloader.to_prepare with Rails.application.config.after_initialize

--- init.rb
+++ init.rb
@@ -18,7 +18,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require File.expand_path('../lib/redmine_hearts/acts_as_heartable.rb', __FILE__)
-require File.expand_path('../lib/redmine_hearts/issue_query.rb', __FILE__)
 require File.expand_path('../lib/redmine_hearts/redmine_heartable_patch.rb', __FILE__)
 require File.expand_path('../lib/redmine_hearts/view_hook.rb', __FILE__)
 
@@ -36,3 +35,32 @@ Redmine::Plugin.register :redmine_hearts do
   menu :project_menu, :hearts, {:controller => :hearts, :action => :index}, :param => :project_id, :caption => :hearts_link_label
 end
 
+if Rails.version > '6.0' && Rails.autoloaders.zeitwerk_enabled?
+  Rails.application.config.after_initialize do
+    IssueQuery.add_available_column(
+      QueryAssociationColumn.new(:hearts, :count,
+                                 :caption => :hearts_link_label,
+                                 :default_order => 'desc',
+                                 :sortable => lambda {
+                                   query_str = Heart.where(:heartable_type => Issue, :heartable_id => "9999").
+                                     select("COUNT(*)").
+                                     to_sql.sub("9999", "#{Issue.table_name}.id")
+                                   "(#{query_str})"
+                                 })
+    )
+  end
+else
+  ActiveSupport::Reloader.to_prepare do
+    IssueQuery.add_available_column(
+      QueryAssociationColumn.new(:hearts, :count,
+                                 :caption => :hearts_link_label,
+                                 :default_order => 'desc',
+                                 :sortable => lambda {
+                                   query_str = Heart.where(:heartable_type => Issue, :heartable_id => "9999").
+                                     select("COUNT(*)").
+                                     to_sql.sub("9999", "#{Issue.table_name}.id")
+                                   "(#{query_str})"
+                                 })
+    )
+  end
+end
